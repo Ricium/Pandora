715729c48fb64c82f2e6c0f91c47c4c562883b3c
Ricium <rm.awsum@gmail.com>
2017-10-13 21:35:15 +0200

    Initial

---
+++ Pandora/Pandora.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Pandora.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
